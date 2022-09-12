7bec6ed52e29cf749fe0580356e6747bb3a6cd81
Dewei <tan.dewei@hotmail.com>
2022-09-12 10:56:04 +0800

    update

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ require("dotenv").config;
 const express = require("express");
 const app = express();
 const port = process.env.PORT ?? 4000;
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+app.use(morgan("dev"));
+
+mongoURI = process.env.mongoURI;
+
+mongoose.connect(mongoURI, {}, () => {
+  console.log("The connection with mongod is established");
+});
 
 app.get("/", (req, res) => {
   res.send({ msg: "Holidays!" });
