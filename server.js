7078b14f3d02de660806f6bb391829ee26792561
Dewei <tan.dewei@hotmail.com>
2022-09-12 11:53:42 +0800

    added eslint and cors

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const app = express();
 const port = process.env.PORT ?? 4000;
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
+//middleware
 app.use(morgan("dev"));
+app.use(cors());
 
-mongoURI = process.env.mongoURI;
+const mongoURI = process.env.mongoURI;
 
 mongoose.connect(mongoURI, {}, () => {
   console.log("The connection with mongod is established");
 });
+
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("connected", () =>
+  console.log("mongo connected: ", mongoURI)
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
 
 app.get("/", (req, res) => {
   res.send({ msg: "Holidays!" });
